66de3abd30321e2735010e1140e4d973a26450a5
Ethan <ethangaba@.com>
2022-11-07 11:49:52 +1300

    skeletons done

--- build-IsraelEthanTaehgCS106point2-Desktop_Qt_6_3_1_MinGW_64_bit-Debug/ui_mainwindow.h
+++ build-IsraelEthanTaehgCS106point2-Desktop_Qt_6_3_1_MinGW_64_bit-Debug/ui_mainwindow.h
@@ -17,6 +17,7 @@
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenuBar>
 #include <QtWidgets/QPushButton>
+#include <QtWidgets/QScrollArea>
 #include <QtWidgets/QSlider>
 #include <QtWidgets/QSpacerItem>
 #include <QtWidgets/QStatusBar>
@@ -32,34 +33,49 @@ public:
     QVBoxLayout *verticalLayout;
     QHBoxLayout *NavBar;
     QSpacerItem *horizontalSpacer_5;
-    QLabel *label;
-    QPushButton *pushButton;
-    QLabel *label_2;
-    QPushButton *pushButton_2;
-    QLabel *label_3;
-    QPushButton *pushButton_3;
-    QLabel *label_4;
-    QPushButton *pushButton_4;
+    QLabel *IconH;
+    QPushButton *homeButton;
+    QLabel *IconG;
+    QPushButton *genresButton;
+    QLabel *IconP;
+    QPushButton *popularButton;
+    QLabel *IconR;
+    QPushButton *reservesButton;
     QSpacerItem *verticalSpacer_2;
     QSpacerItem *horizontalSpacer;
-    QLabel *label_5;
-    QPushButton *pushButton_5;
+    QLabel *labelA;
+    QPushButton *AccountButton;
     QSpacerItem *horizontalSpacer_2;
     QVBoxLayout *verticalLayout_2;
-    QLabel *label_6;
+    QLabel *Title;
     QHBoxLayout *horizontalLayout_2;
     QSpacerItem *horizontalSpacer_3;
-    QLineEdit *lineEdit_2;
+    QLineEdit *searchBar;
     QSpacerItem *horizontalSpacer_4;
     QSpacerItem *verticalSpacer;
     QVBoxLayout *verticalLayout_3;
+    QScrollArea *scrollArea;
+    QWidget *scrollAreaWidgetContents;
+    QWidget *widget;
     QHBoxLayout *horizontalLayout;
-    QLabel *Book5;
-    QLabel *Book4;
-    QLabel *Book3;
-    QLabel *Book2;
+    QSpacerItem *horizontalSpacer_14;
     QLabel *Book1;
-    QSlider *horizontalSlider;
+    QSpacerItem *horizontalSpacer_6;
+    QLabel *Book2;
+    QSpacerItem *horizontalSpacer_7;
+    QLabel *Book3;
+    QSpacerItem *horizontalSpacer_8;
+    QLabel *Book4;
+    QSpacerItem *horizontalSpacer_9;
+    QLabel *Book5;
+    QSpacerItem *horizontalSpacer_10;
+    QLabel *Book6;
+    QSpacerItem *horizontalSpacer_11;
+    QLabel *Book7;
+    QSpacerItem *horizontalSpacer_12;
+    QLabel *Book8;
+    QSpacerItem *horizontalSpacer_13;
+    QSlider *bookScroller;
     QSpacerItem *verticalSpacer_3;
     QMenuBar *menubar;
     QStatusBar *statusbar;
@@ -68,7 +84,7 @@ public:
     {
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
-        MainWindow->resize(757, 482);
+        MainWindow->resize(760, 511);
         centralwidget = new QWidget(MainWindow);
         centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
         verticalLayout = new QVBoxLayout(centralwidget);
@@ -79,53 +95,53 @@ public:
 
         NavBar->addItem(horizontalSpacer_5);
 
-        label = new QLabel(centralwidget);
-        label->setObjectName(QString::fromUtf8("label"));
-        label->setMaximumSize(QSize(16777215, 50));
+        IconH = new QLabel(centralwidget);
+        IconH->setObjectName(QString::fromUtf8("IconH"));
+        IconH->setMaximumSize(QSize(16777215, 50));
 
-        NavBar->addWidget(label);
+        NavBar->addWidget(IconH);
 
-        pushButton = new QPushButton(centralwidget);
-        pushButton->setObjectName(QString::fromUtf8("pushButton"));
-        pushButton->setMaximumSize(QSize(16777215, 50));
+        homeButton = new QPushButton(centralwidget);
+        homeButton->setObjectName(QString::fromUtf8("homeButton"));
+        homeButton->setMaximumSize(QSize(16777215, 50));
 
-        NavBar->addWidget(pushButton);
+        NavBar->addWidget(homeButton);
 
-        label_2 = new QLabel(centralwidget);
-        label_2->setObjectName(QString::fromUtf8("label_2"));
-        label_2->setMaximumSize(QSize(16777215, 50));
+        IconG = new QLabel(centralwidget);
+        IconG->setObjectName(QString::fromUtf8("IconG"));
+        IconG->setMaximumSize(QSize(16777215, 50));
 
-        NavBar->addWidget(label_2);
+        NavBar->addWidget(IconG);
 
-        pushButton_2 = new QPushButton(centralwidget);
-        pushButton_2->setObjectName(QString::fromUtf8("pushButton_2"));
-        pushButton_2->setMaximumSize(QSize(16777215, 50));
+        genresButton = new QPushButton(centralwidget);
+        genresButton->setObjectName(QString::fromUtf8("genresButton"));
+        genresButton->setMaximumSize(QSize(16777215, 50));
 
-        NavBar->addWidget(pushButton_2);
+        NavBar->addWidget(genresButton);
 
-        label_3 = new QLabel(centralwidget);
-        label_3->setObjectName(QString::fromUtf8("label_3"));
-        label_3->setMaximumSize(QSize(16777215, 50));
+        IconP = new QLabel(centralwidget);
+        IconP->setObjectName(QString::fromUtf8("IconP"));
+        IconP->setMaximumSize(QSize(16777215, 50));
 
-        NavBar->addWidget(label_3);
+        NavBar->addWidget(IconP);
 
-        pushButton_3 = new QPushButton(centralwidget);
-        pushButton_3->setObjectName(QString::fromUtf8("pushButton_3"));
-        pushButton_3->setMaximumSize(QSize(16777215, 50));
+        popularButton = new QPushButton(centralwidget);
+        popularButton->setObjectName(QString::fromUtf8("popularButton"));
+        popularButton->setMaximumSize(QSize(16777215, 50));
 
-        NavBar->addWidget(pushButton_3);
+        NavBar->addWidget(popularButton);
 
-        label_4 = new QLabel(centralwidget);
-        label_4->setObjectName(QString::fromUtf8("label_4"));
-        label_4->setMaximumSize(QSize(16777215, 50));
+        IconR = new QLabel(centralwidget);
+        IconR->setObjectName(QString::fromUtf8("IconR"));
+        IconR->setMaximumSize(QSize(16777215, 50));
 
-        NavBar->addWidget(label_4);
+        NavBar->addWidget(IconR);
 
-        pushButton_4 = new QPushButton(centralwidget);
-        pushButton_4->setObjectName(QString::fromUtf8("pushButton_4"));
-        pushButton_4->setMaximumSize(QSize(16777215, 50));
+        reservesButton = new QPushButton(centralwidget);
+        reservesButton->setObjectName(QString::fromUtf8("reservesButton"));
+        reservesButton->setMaximumSize(QSize(16777215, 50));
 
-        NavBar->addWidget(pushButton_4);
+        NavBar->addWidget(reservesButton);
 
         verticalSpacer_2 = new QSpacerItem(20, 50, QSizePolicy::Minimum, QSizePolicy::Maximum);
 
@@ -135,17 +151,17 @@ public:
 
         NavBar->addItem(horizontalSpacer);
 
-        label_5 = new QLabel(centralwidget);
-        label_5->setObjectName(QString::fromUtf8("label_5"));
-        label_5->setMaximumSize(QSize(16777215, 50));
+        labelA = new QLabel(centralwidget);
+        labelA->setObjectName(QString::fromUtf8("labelA"));
+        labelA->setMaximumSize(QSize(16777215, 50));
 
-        NavBar->addWidget(label_5);
+        NavBar->addWidget(labelA);
 
-        pushButton_5 = new QPushButton(centralwidget);
-        pushButton_5->setObjectName(QString::fromUtf8("pushButton_5"));
-        pushButton_5->setMaximumSize(QSize(16777215, 50));
+        AccountButton = new QPushButton(centralwidget);
+        AccountButton->setObjectName(QString::fromUtf8("AccountButton"));
+        AccountButton->setMaximumSize(QSize(16777215, 50));
 
-        NavBar->addWidget(pushButton_5);
+        NavBar->addWidget(AccountButton);
 
         horizontalSpacer_2 = new QSpacerItem(20, 20, QSizePolicy::Maximum, QSizePolicy::Minimum);
 
@@ -156,15 +172,15 @@ public:
 
         verticalLayout_2 = new QVBoxLayout();
         verticalLayout_2->setObjectName(QString::fromUtf8("verticalLayout_2"));
-        label_6 = new QLabel(centralwidget);
-        label_6->setObjectName(QString::fromUtf8("label_6"));
+        Title = new QLabel(centralwidget);
+        Title->setObjectName(QString::fromUtf8("Title"));
         QFont font;
         font.setPointSize(22);
-        label_6->setFont(font);
-        label_6->setScaledContents(false);
-        label_6->setAlignment(Qt::AlignCenter);
+        Title->setFont(font);
+        Title->setScaledContents(false);
+        Title->setAlignment(Qt::AlignCenter);
 
-        verticalLayout_2->addWidget(label_6);
+        verticalLayout_2->addWidget(Title);
 
         horizontalLayout_2 = new QHBoxLayout();
         horizontalLayout_2->setObjectName(QString::fromUtf8("horizontalLayout_2"));
@@ -172,10 +188,11 @@ public:
 
         horizontalLayout_2->addItem(horizontalSpacer_3);
 
-        lineEdit_2 = new QLineEdit(centralwidget);
-        lineEdit_2->setObjectName(QString::fromUtf8("lineEdit_2"));
+        searchBar = new QLineEdit(centralwidget);
+        searchBar->setObjectName(QString::fromUtf8("searchBar"));
+        searchBar->setAlignment(Qt::AlignCenter);
 
-        horizontalLayout_2->addWidget(lineEdit_2);
+        horizontalLayout_2->addWidget(searchBar);
 
         horizontalSpacer_4 = new QSpacerItem(200, 20, QSizePolicy::Minimum, QSizePolicy::Minimum);
 
@@ -193,51 +210,121 @@ public:
 
         verticalLayout_3 = new QVBoxLayout();
         verticalLayout_3->setObjectName(QString::fromUtf8("verticalLayout_3"));
-        horizontalLayout = new QHBoxLayout();
+        scrollArea = new QScrollArea(centralwidget);
+        scrollArea->setObjectName(QString::fromUtf8("scrollArea"));
+        scrollArea->setMinimumSize(QSize(740, 205));
+        scrollArea->setWidgetResizable(true);
+        scrollAreaWidgetContents = new QWidget();
+        scrollAreaWidgetContents->setObjectName(QString::fromUtf8("scrollAreaWidgetContents"));
+        scrollAreaWidgetContents->setGeometry(QRect(0, 0, 738, 203));
+        scrollAreaWidgetContents->setMinimumSize(QSize(0, 200));
+        widget = new QWidget(scrollAreaWidgetContents);
+        widget->setObjectName(QString::fromUtf8("widget"));
+        widget->setGeometry(QRect(0, 0, 1478, 202));
+        horizontalLayout = new QHBoxLayout(widget);
         horizontalLayout->setObjectName(QString::fromUtf8("horizontalLayout"));
-        Book5 = new QLabel(centralwidget);
-        Book5->setObjectName(QString::fromUtf8("Book5"));
-        Book5->setMinimumSize(QSize(0, 200));
-        Book5->setAlignment(Qt::AlignCenter);
+        horizontalLayout->setContentsMargins(0, 0, 0, 0);
+        horizontalSpacer_14 = new QSpacerItem(20, 20, QSizePolicy::Minimum, QSizePolicy::Minimum);
 
-        horizontalLayout->addWidget(Book5);
+        horizontalLayout->addItem(horizontalSpacer_14);
 
-        Book4 = new QLabel(centralwidget);
-        Book4->setObjectName(QString::fromUtf8("Book4"));
-        Book4->setMinimumSize(QSize(0, 200));
-        Book4->setAlignment(Qt::AlignCenter);
-
-        horizontalLayout->addWidget(Book4);
-
-        Book3 = new QLabel(centralwidget);
-        Book3->setObjectName(QString::fromUtf8("Book3"));
-        Book3->setMinimumSize(QSize(0, 200));
-        Book3->setAlignment(Qt::AlignCenter);
-
-        horizontalLayout->addWidget(Book3);
-
-        Book2 = new QLabel(centralwidget);
-        Book2->setObjectName(QString::fromUtf8("Book2"));
-        Book2->setMinimumSize(QSize(0, 200));
-        Book2->setAlignment(Qt::AlignCenter);
-
-        horizontalLayout->addWidget(Book2);
-
-        Book1 = new QLabel(centralwidget);
+        Book1 = new QLabel(widget);
         Book1->setObjectName(QString::fromUtf8("Book1"));
-        Book1->setMinimumSize(QSize(0, 200));
+        Book1->setMinimumSize(QSize(150, 200));
         Book1->setAlignment(Qt::AlignCenter);
 
         horizontalLayout->addWidget(Book1);
 
+        horizontalSpacer_6 = new QSpacerItem(20, 20, QSizePolicy::Minimum, QSizePolicy::Minimum);
 
-        verticalLayout_3->addLayout(horizontalLayout);
+        horizontalLayout->addItem(horizontalSpacer_6);
 
-        horizontalSlider = new QSlider(centralwidget);
-        horizontalSlider->setObjectName(QString::fromUtf8("horizontalSlider"));
-        horizontalSlider->setOrientation(Qt::Horizontal);
+        Book2 = new QLabel(widget);
+        Book2->setObjectName(QString::fromUtf8("Book2"));
+        Book2->setMinimumSize(QSize(150, 200));
+        Book2->setAlignment(Qt::AlignCenter);
 
-        verticalLayout_3->addWidget(horizontalSlider);
+        horizontalLayout->addWidget(Book2);
+
+        horizontalSpacer_7 = new QSpacerItem(20, 20, QSizePolicy::Minimum, QSizePolicy::Minimum);
+
+        horizontalLayout->addItem(horizontalSpacer_7);
+
+        Book3 = new QLabel(widget);
+        Book3->setObjectName(QString::fromUtf8("Book3"));
+        Book3->setMinimumSize(QSize(150, 200));
+        Book3->setAlignment(Qt::AlignCenter);
+
+        horizontalLayout->addWidget(Book3);
+
+        horizontalSpacer_8 = new QSpacerItem(20, 20, QSizePolicy::Minimum, QSizePolicy::Minimum);
+
+        horizontalLayout->addItem(horizontalSpacer_8);
+
+        Book4 = new QLabel(widget);
+        Book4->setObjectName(QString::fromUtf8("Book4"));
+        Book4->setMinimumSize(QSize(150, 200));
+        Book4->setAlignment(Qt::AlignCenter);
+
+        horizontalLayout->addWidget(Book4);
+
+        horizontalSpacer_9 = new QSpacerItem(20, 20, QSizePolicy::Minimum, QSizePolicy::Minimum);
+
+        horizontalLayout->addItem(horizontalSpacer_9);
+
+        Book5 = new QLabel(widget);
+        Book5->setObjectName(QString::fromUtf8("Book5"));
+        Book5->setMinimumSize(QSize(150, 200));
+        Book5->setAlignment(Qt::AlignCenter);
+
+        horizontalLayout->addWidget(Book5);
+
+        horizontalSpacer_10 = new QSpacerItem(20, 20, QSizePolicy::Minimum, QSizePolicy::Minimum);
+
+        horizontalLayout->addItem(horizontalSpacer_10);
+
+        Book6 = new QLabel(widget);
+        Book6->setObjectName(QString::fromUtf8("Book6"));
+        Book6->setMinimumSize(QSize(150, 200));
+        Book6->setAlignment(Qt::AlignCenter);
+
+        horizontalLayout->addWidget(Book6);
+
+        horizontalSpacer_11 = new QSpacerItem(20, 20, QSizePolicy::Minimum, QSizePolicy::Minimum);
+
+        horizontalLayout->addItem(horizontalSpacer_11);
+
+        Book7 = new QLabel(widget);
+        Book7->setObjectName(QString::fromUtf8("Book7"));
+        Book7->setMinimumSize(QSize(150, 200));
+        Book7->setAlignment(Qt::AlignCenter);
+
+        horizontalLayout->addWidget(Book7);
+
+        horizontalSpacer_12 = new QSpacerItem(20, 20, QSizePolicy::Minimum, QSizePolicy::Minimum);
+
+        horizontalLayout->addItem(horizontalSpacer_12);
+
+        Book8 = new QLabel(widget);
+        Book8->setObjectName(QString::fromUtf8("Book8"));
+        Book8->setMinimumSize(QSize(150, 200));
+        Book8->setAlignment(Qt::AlignCenter);
+
+        horizontalLayout->addWidget(Book8);
+
+        horizontalSpacer_13 = new QSpacerItem(20, 20, QSizePolicy::Minimum, QSizePolicy::Minimum);
+
+        horizontalLayout->addItem(horizontalSpacer_13);
+
+        scrollArea->setWidget(scrollAreaWidgetContents);
+
+        verticalLayout_3->addWidget(scrollArea);
+
+        bookScroller = new QSlider(centralwidget);
+        bookScroller->setObjectName(QString::fromUtf8("bookScroller"));
+        bookScroller->setOrientation(Qt::Horizontal);
+
+        verticalLayout_3->addWidget(bookScroller);
 
 
         verticalLayout->addLayout(verticalLayout_3);
@@ -249,7 +336,7 @@ public:
         MainWindow->setCentralWidget(centralwidget);
         menubar = new QMenuBar(MainWindow);
         menubar->setObjectName(QString::fromUtf8("menubar"));
-        menubar->setGeometry(QRect(0, 0, 757, 21));
+        menubar->setGeometry(QRect(0, 0, 760, 21));
         MainWindow->setMenuBar(menubar);
         statusbar = new QStatusBar(MainWindow);
         statusbar->setObjectName(QString::fromUtf8("statusbar"));
@@ -263,22 +350,26 @@ public:
     void retranslateUi(QMainWindow *MainWindow)
     {
         MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
-        label->setText(QCoreApplication::translate("MainWindow", "IconH", nullptr));
-        pushButton->setText(QCoreApplication::translate("MainWindow", "Home", nullptr));
-        label_2->setText(QCoreApplication::translate("MainWindow", "IconG", nullptr));
-        pushButton_2->setText(QCoreApplication::translate("MainWindow", "Genres", nullptr));
-        label_3->setText(QCoreApplication::translate("MainWindow", "IconP", nullptr));
-        pushButton_3->setText(QCoreApplication::translate("MainWindow", "Popular", nullptr));
-        label_4->setText(QCoreApplication::translate("MainWindow", "IconR", nullptr));
-        pushButton_4->setText(QCoreApplication::translate("MainWindow", "Reserves", nullptr));
-        label_5->setText(QCoreApplication::translate("MainWindow", "IconA", nullptr));
-        pushButton_5->setText(QCoreApplication::translate("MainWindow", "Login/Register", nullptr));
-        label_6->setText(QCoreApplication::translate("MainWindow", "Welcome to 106 Library system", nullptr));
-        Book5->setText(QCoreApplication::translate("MainWindow", "Book5", nullptr));
-        Book4->setText(QCoreApplication::translate("MainWindow", "book4", nullptr));
-        Book3->setText(QCoreApplication::translate("MainWindow", "Book3", nullptr));
-        Book2->setText(QCoreApplication::translate("MainWindow", "Book2", nullptr));
+        IconH->setText(QCoreApplication::translate("MainWindow", "IconH", nullptr));
+        homeButton->setText(QCoreApplication::translate("MainWindow", "Home", nullptr));
+        IconG->setText(QCoreApplication::translate("MainWindow", "IconG", nullptr));
+        genresButton->setText(QCoreApplication::translate("MainWindow", "Genres", nullptr));
+        IconP->setText(QCoreApplication::translate("MainWindow", "IconP", nullptr));
+        popularButton->setText(QCoreApplication::translate("MainWindow", "Popular", nullptr));
+        IconR->setText(QCoreApplication::translate("MainWindow", "IconR", nullptr));
+        reservesButton->setText(QCoreApplication::translate("MainWindow", "Reserves", nullptr));
+        labelA->setText(QCoreApplication::translate("MainWindow", "IconA", nullptr));
+        AccountButton->setText(QCoreApplication::translate("MainWindow", "Login/Register", nullptr));
+        Title->setText(QCoreApplication::translate("MainWindow", "Welcome to 106 Library system", nullptr));
+        searchBar->setText(QCoreApplication::translate("MainWindow", "Search...", nullptr));
         Book1->setText(QCoreApplication::translate("MainWindow", "Book1", nullptr));
+        Book2->setText(QCoreApplication::translate("MainWindow", "Book2", nullptr));
+        Book3->setText(QCoreApplication::translate("MainWindow", "Book3", nullptr));
+        Book4->setText(QCoreApplication::translate("MainWindow", "book4", nullptr));
+        Book5->setText(QCoreApplication::translate("MainWindow", "Book5", nullptr));
+        Book6->setText(QCoreApplication::translate("MainWindow", "Book6", nullptr));
+        Book7->setText(QCoreApplication::translate("MainWindow", "Book7", nullptr));
+        Book8->setText(QCoreApplication::translate("MainWindow", "Book8", nullptr));
     } // retranslateUi
 
 };
