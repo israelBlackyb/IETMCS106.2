a66bd20af40de41bd72ec2501ac44d118ba7483d
Taehg <taehg7@gmail.com>
2022-11-09 10:11:39 +1300

    Revert "Merge branch 'Taehg2' into prototype"
    This reverts commit 31356b0b1319a9ed45405136ddf8dc66f11710dd.

--- IsraelEthanTaehgCS106point2/adminbookpage.cpp
+++ IsraelEthanTaehgCS106point2/adminbookpage.cpp
@@ -19,8 +19,6 @@ AdminBookPage::AdminBookPage(QWidget *parent) :
     QPushButton* cancelBtn = new QPushButton(editBox);
     cancelBtn->setText("Cancel");
     saveBtn->connect(cancelBtn, SIGNAL(clicked()), this, SLOT(cancelClicked()));
-    saveBtn->connect(cancelBtn, SIGNAL(clicked()), this, SLOT(cancelClicked()));
-    cancelBtn->setText("Cancel");
 
         //Create instance of vertical layout group, horizontal layout group, and spacer
         //Populate the layout groups with each element created
--- IsraelEthanTaehgCS106point2/mainwindow.h
+++ IsraelEthanTaehgCS106point2/mainwindow.h
@@ -17,7 +17,11 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+
 private slots:
+    void on_loginReg_clicked();
+
+    void on_homeButton_clicked();
 
 private:
     Ui::MainWindow *ui;
